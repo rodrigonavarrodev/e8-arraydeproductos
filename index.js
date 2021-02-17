f0af4d4feeeb2b367626e7daee2f1243fedebc8c
rodrigonavarrodev <rodrigonavarrodg@gmail.com>
2021-02-17 17:08:59 -0300

    pug

--- index.js
+++ index.js
@@ -2,40 +2,24 @@ const express = require('express')
 const app = express()
 const router = express.Router()
 const path = require('path');
-const handlebars = require('express-handlebars')
-
 
 app.use(express.json())
 app.use(express.urlencoded({extended: true}))
 
-
-app.engine(
-    'hbs',
-    handlebars({
-        extname: '.hbs',
-        defaultLayout: 'index.hbs',
-        layoutsDir: __dirname + '/views/layouts',
-        partialsDir: __dirname + '/views/partials/'
-    })
-)
+app.set('view engine', 'pug')
+app.set('views', './views')
+app.use(express.static('public'))
 
 
 let productos = []
 
 router.get('/', function (req, res) {
-    //res.sendFile(path.join(__dirname, '/public', 'index.html'));
-    res.render('form')
+    res.render('form.pug',{})
 })
 
 
-
 router.get('/productos', (req, res) => {
-    if (productos.length === 0) {
-    res.render('productos', {productos: [
-        {titulo: "No hay productos para mostrar"}
-    ], listExists: true})
-    }
-    res.render('productos', {productos, listExists: true})
+    res.render('productos.pug', {productos: productos})
 })
  
 router.get('/productos/:id', (req, res) => {
@@ -45,7 +29,7 @@ router.get('/productos/:id', (req, res) => {
         res.status(404).json("El producto no existe")
         
     }
-    res.render('productos', {productos, listExists: true})
+    res.render('productos.pug', {productos: productos})
 })
 
 router.post('/productos', (req, res) => {
@@ -57,8 +41,8 @@ router.post('/productos', (req, res) => {
         id: productos.length+1
     }
     productos.push(producto)
-    //res.send([...productos, producto])
-    res.render('productos', {productos, listExists: true})
+    res.render('productos.pug', {productos: productos})
+
 })
 
 app.put('/productos/:id', (req, res) => {
@@ -71,7 +55,7 @@ app.put('/productos/:id', (req, res) => {
     producto.titulo = titulo
     producto.precio = precio
     producto.foto = foto
-    res.render('productos', {productos, listExists: true})
+    res.render('productos.pug', {productos: productos})
 }) 
 
 app.delete('/productos/:id', (req, res) => {
@@ -81,15 +65,11 @@ app.delete('/productos/:id', (req, res) => {
         res.status(404).json("El producto no existe")
     }
     productos = productos.filter( producto => producto.id === id)
-    res.render('productos', {productos, listExists: true})
+    res.render('productos.pug', {productos: productos})
 })
 
 
 app.use('/api', router)
-
-app.set('view engine', 'hbs')
-app.set('views', './views')
-app.use(express.static('public'))
 
 app.listen(8080, () => {
     console.log("El servidor esta corriendo en el puerto 8080");
