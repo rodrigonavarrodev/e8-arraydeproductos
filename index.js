5c97cfd799e917bc23fd605f3615b89b18f91188
rodrigonavarrodev <rodrigonavarrodg@gmail.com>
2021-03-01 17:10:30 -0300

    entregable 13

--- index.js
+++ index.js
@@ -3,35 +3,35 @@ const express = require('express')
 const app = express()
 const server = require('http').createServer(app);
 const io = require('socket.io')(server)
+const fs = require('fs');
+
 
 const router = express.Router()
 const path = require('path');
-const handlebars = require('express-handlebars')
 
 
 app.use(express.json())
 app.use(express.urlencoded({extended: true}))
 
 
-app.engine(
-    'hbs',
-    handlebars({
-        extname: '.hbs',
-        defaultLayout: 'index.hbs',
-        layoutsDir: __dirname + '/views/layouts',
-        partialsDir: __dirname + '/views/partials/'
-    })
-)
+const m = './mensajes.txt'
 
 
-let productos = []
+let productos = [
+    { titulo: 'Heladera', precio: '10000', foto: 'https://cdn3.iconfinder.com/data/icons/smart-home-set/132/Icon_refrigerator-64.png' },
+    { titulo: 'Horno', precio: '5000', foto: 'https://cdn3.iconfinder.com/data/icons/smart-home-set/132/Icon_refrigerator-64.png'}
+]
+
+let messages = [
+    { email: 'mail@mail.com', fecha: '28/02/2021', text: 'Hola, soy juan'},
+    { email: 'test@mail.com', fecha: '28/02/2021', text: 'Hola juan, yo soy Rodrigo'}
+]
 
 app.get('/', function (req, res) {
-    //res.sendFile(path.join(__dirname, '/public', 'index.html'));
-    res.render('form')
+    console.log('ruta principal');
+    res.sendFile(path.join(__dirname, '/public', 'index.html'));
+
 })
-
-
 
 app.get('/productos', (req, res) => {
     if (productos.length === 0) {
@@ -90,9 +90,6 @@ app.delete('/productos/:id', (req, res) => {
 
 
 //app.use('/api', router)
-
-app.set('view engine', 'hbs')
-app.set('views', './views')
 app.use(express.static('public'))
 
 
@@ -100,32 +97,25 @@ server.listen(8080, () => {
     console.log("El servidor esta corriendo en el puerto 8080");
 })
 
-//escuchamos un evento de conexion
-/* io.on('connection', (socket) => {
-    console.log(socket.id);
-
-    socket.broadcast.emit('productos', {productos});
-
-    socket.on('product', (message) => {
-        const { titulo, precio, foto} = message;
-
-        const producto = {
-            titulo,
-            precio,
-            foto,
-            id: productos.length+1
-        }
-
-        products.push(producto);
-    })  
-}) */
-
-
 io.on('connection', (socket) => {
-    socket.broadcast.emit('productos', 'Desde el server')
-    //console.log(socket.id)
-    socket.on('producto', (mensaje) => {
-        console.log(mensaje);
-        io.emit('producto', mensaje)
+    console.log('se contecto el ciente', socket.id)
+    //emito los mensajes ya guardados
+
+    const m = fs.readFileSync('./mensajes.txt', 'utf-8')
+    const allm = JSON.parse(m)
+    console.log(m)
+
+    socket.emit('messages', allm)
+
+    //escucho los mensajes que envian desde el cliente
+    socket.on('new-message', (data) => {
+        //aca deberia guardar la data que recibo en el txt de mensajes
+        //fs.appendFileSync('./mensajes.txt', data)
+        allm.push(data);
+        io.sockets.emit('messages', allm)
     })
+  
+|   //emito los productos ya guardados
+    socket.emit('producto', productos)
+
 })
---
+++ public/main.js
@@ -0,0 +1,47 @@
+
+const socket = io()
+
+    const form = document.getElementById('form')
+    const input = document.getElementById('input')
+    let productDom = document.getElementById('productDom')
+    
+    
+   //esuchar el evento de cuando alguien aprete enviar
+    form.addEventListener('submit', (event) => {
+        //para no recargar la pagina
+        event.preventDefault()
+        if(input.value) {
+            socket.emit('producto', input.value)
+            input.value = ''
+        }
+    })
+
+    //renderizar mensajes que vienen del servidor
+    function render (data) {
+        let html = data.map(function(elem, index) {
+            return(`<div>
+            <strong style="color:blue">${elem.email}</strong>:
+            <em style="color:brown">${elem.fecha}</em>
+            <em style="color:green", "font-style: italic">${elem.text}</em>
+            </div>`)
+        }).join(' ');
+        document.getElementById('messages').innerHTML = html;
+        }
+        socket.on('messages', function(data) {
+            render (data)
+            console.log(data);
+        })
+
+      //definimos la funcion para enviar el mensaje al servidor
+    function addMessage(e) {
+        
+        let mensaje = {
+            email: document.getElementById('email').value,
+            fecha: new Date(),
+            text: document.getElementById('texto').value
+        }
+        socket.emit('new-message', mensaje)
+        return false
+    }
+  
+    
