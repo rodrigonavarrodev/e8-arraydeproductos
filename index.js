432ed31fe15d57904d00d9cb459f8f0149ef6a2e
Rodrigo Alejandro Navarro <rnavarro@accionpoint.com>
2021-04-28 16:52:14 -0300

    26-Passport

--- index.js
+++ index.js
@@ -1,115 +1,155 @@
 const express = require("express");
 const app = express();
 const router = express.Router();
-const path = require("path");
-const cookieParser = require("cookie-parser")
-const session = require('express-session')
 
-//Persistencia por mongoDB
-const MongoStore = require('connect-mongo')
+//PASSPORT
+const bcrypt = require("bcrypt");
+const passport = require("passport");
+const localStrategy = require("passport-local").Strategy;
 
+const User = require('./models/user')
 
-
-const handlebars = require("express-handlebars");
-const { db } = require("./models/producto");
-
-require("dotenv").config();
-require("./database");
-
-app.use(cookieParser())
-app.use(session({
-
-  store: MongoStore.create ({ mongoUrl: 'mongodb://localhost/sesiones'}),
-
+app.use(require('cookie-parser')())
+app.use(require('express-session')({
   secret: 'secret',
-  resave: false,
-  saveUninitialized: false,
   cookie: {
-    maxAge: 40000
-  }
+    httpOnly: false,
+    secure: false,
+    maxAge: 60000
+  },
+  rolling: true,
+  resave: true,
+  saveUninitialized: false
 }))
 
+app.use(passport.initialize())
+app.use(passport.session())
 
+const bodyParser = require("body-parser");
+
+const cookieParser = require("cookie-parser");
+
+
+const { db } = require("./models/producto");
+require("dotenv").config();
+require("./database");
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.engine(
-  "hbs",
-  handlebars({
-    extname: ".hbs",
-    defaultLayout: "index.hbs",
-    layoutsDir: __dirname + "/views/layouts",
-    partialsDir: __dirname + "/views/partials/",
-  })
+
+const validatePassword = function (user, password) {
+  return bcrypt.compareSync(password, user.password);
+};
+
+const createHash = function(password){
+  return bcrypt.hashSync(password, bcrypt.genSaltSync(10), null)
+}
+
+passport.use(
+  "login",
+  new localStrategy(
+    {
+      passReqToCallback: true,
+    },
+    (req, username, password, cb) => {
+      User.findOne({ username: username }, (err, user) => {
+        if (err) return done(err);
+        if (!user) {
+          console.log("User Not Found with username " + username);
+          return cb(null, false);
+        }
+        if (!validatePassword(user, password)) {
+          console.log("Invalid Password");
+          return cb(null, false);
+        }
+        return cb(null, user);
+      });
+    }
+  )
 );
 
-router.get("/", function (req, res) {
-  //res.sendFile(path.join(__dirname, '/public', 'index.html'));
-  res.render("productos");
-});
-
-/* router.get("/productos/:id", async (req, res) => {
-  const id = req.params.id;
-  let producto = await Producto.findById(id);
-  res.json(producto);
-});
-
-router.post("/productos", async (req, res) => {
-  const item = ({ producto, precio, foto } = new Producto(req.body));
-  let result = await item.save();
-  res.json(item);
-});
-
-router.get("/productos", async (req, res) => {
-  let productos = await Producto.find();
-  res.json(productos);
-}); 
-
-router.delete("/productos/:id", async (req, res) => {
-  const id = req.params.id;
-  let producto = await Producto.findByIdAndDelete(id);
-  res.json(producto);
-}) */
-
-//--------- SESSION -------------------
-
-
-router.post('/login', async (req, res) => {
-  const { username } = req.body
-
-  req.session.username = username
-  req.session.cookie.expires = new Date(Date.now() + 60000)
-  //Verificacion de que el usuario esta logueado con la misma session al actualizar
-  console.log(req.sessionID);
-  res.send(`Bienvenido ${req.session.username}`)
-})
-
-router.get('/logout', async (req, res) => {
-  const username = req.session.username;
-  req.session.destroy(err => {
-    if(!err){
-      res.send(`Hasta luego ${username}`)
+passport.use(
+  "register",
+  new localStrategy(
+    {
+      passReqToCallback: true,
+    },
+    function (req, username, password, cb) {
+      const findOrCreateUser = function () {
+        User.findOne({ username: username }, function (err, user) {
+          if (err) {
+            console.log("Error in SignUp: " + err);
+            return cb(err);
+          }
+          if (user) {
+            console.log("User already exists");
+            return cb(null, false);
+          } else {
+            var newUser = new User();
+            newUser.username = username;
+            newUser.password = createHash(password);
+            newUser.save((err) => {
+              if (err) {
+                console.log("Error in Saving user: " + err);
+                throw err;
+              }
+              console.log("User Registration succesful");
+              return cb(null, newUser);
+            });
+          }
+        });
+      };
+      process.nextTick(findOrCreateUser);
     }
-  })
-})
+  )
+);
+
+passport.serializeUser((user, done) => {
+  done(null, user._id);
+});
+
+passport.deserializeUser((id, done) => {
+  User.findById(id, function (err, user) {
+    done(err, user);
+  });
+});
 
 
+router.get("/", function (req, res) {
+  res.json({ msg: "llegue" });
+});
 
 
+//LOGIN
+app.post(
+  "/login",
+  passport.authenticate("login", { failureRedirect: "/faillogin" }),
+  (req, res) => {
+    res.json('Login OK')
+    //res.redirect("/");
+  }
+);
+
+app.get("/faillogin", (req, res) => {
+  res.json("login-error", {});
+});
+
+//REGISTER
+app.post(
+  "/register",
+  passport.authenticate("register", { failureRedirect: "/failregister" }),
+  (req, res) => {
+    res.json('regiser OK')
+    //res.redirect("/");
+  }
+);
+
+app.get("/failregister", (req, res) => {
+  res.json("register-error", {});
+});
 
 
-
-
-
-//--------- SESSION -------------------
-
-
-app.use("/api", router);
-
-app.set("view engine", "hbs");
-app.set("views", "./views");
-app.use(express.static("public"));
 
 app.listen(8080, () => {
   console.log("El servidor esta corriendo en el puerto 8080");
